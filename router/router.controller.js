600f081876e8da10044d74b10e7c8a32767d3e89
Jovial P Thomas <32453962+jovialp@users.noreply.github.com>
2020-01-06 15:40:28 +0530

    Update router.controller.js

--- router/router.controller.js
+++ router/router.controller.js
@@ -3,6 +3,6 @@ const app = express();
 const orderRoute = require('./order.router');
 const productRoute = require('./product.router');
 app.use('/', productRoute);
-app.use('/order',orderRoute);
+app.use('/order', orderRoute);
 
 module.exports = app;
