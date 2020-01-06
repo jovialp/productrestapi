a935f30597d807450cad3b1e2e8418fc0b7f001b
Jovial P Thomas <32453962+jovialp@users.noreply.github.com>
2020-01-06 13:23:29 +0530

    Update order.router.js

--- router/order.router.js
+++ router/order.router.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const orderRoute = express.Router();
 
+const Order = require('../model/order.model');
+
+orderRoute.route('/').get(function (req, res) {
+    Order.find(function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+orderRoute.route('/').post(function (req, res) {
+    let order = new Order(req.body);
+
+    order.save().then(obj => {
+        res.status(200).json({ message: 'Product added successfully' });
+    }).catch(err => {
+        res.status(400).json({ message: 'unable to save the product' });
+    });
+});
