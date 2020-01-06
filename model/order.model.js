ec2ed505de4b4ab24e83e5291121dcacd4f731bf
Jovial P Thomas <32453962+jovialp@users.noreply.github.com>
2020-01-06 15:44:44 +0530

    Update order.model.js

--- model/order.model.js
+++ model/order.model.js
@@ -1,22 +1,23 @@
-//db schema
+// db schema
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//declaring collections chema property and names
+// declaring collection schema property and names
 
 let Order = new Schema(
-    {
-        //schema properties
-        username: { type: String },
-        address: { type: String },
-        mobile: { type: String },
-        orderinfo: { type: Array },
-        orderstatus: { type: String }
-        orderdate: { type: String }
-    },
-    {
-        collection: 'order'
-    }
+  {
+    // schema property: values
+    username: { type: String },
+    address: { type: String },
+    mobile: { type: String },
+    orderinfo: { type: Array },
+    orderstatus: { type: String },
+    orderdate: { type: String }
+  },
+  {
+    collection: 'order'
+  }
 );
-module.exports = mongoose.model('Order', Order);
+
+module.exports = mongoose.model('Order',Order);
