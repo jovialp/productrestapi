037838d5cb20d9df689e757f3cdf2a51b2513123
Jovial P Thomas <32453962+jovialp@users.noreply.github.com>
2020-01-06 13:14:36 +0530

    Create order.model.js

---
+++ model/order.model.js
@@ -0,0 +1,22 @@
+//db schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//declaring collections chema property and names
+
+let Order = new Schema(
+    {
+        //schema properties
+        username: { type: String },
+        address: { type: String },
+        mobile: { type: String },
+        orderinfo: { type: Array },
+        orderstatus: { type: String }
+        orderdate: { type: Date }
+    },
+    {
+        collection: 'order'
+    }
+);
+module.exports = mongoose.model('Order', Order);
