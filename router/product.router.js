c0d5642d6e95700ee193b51a469072e3d3f039a7
Jovial P Thomas <32453962+jovialp@users.noreply.github.com>
2020-01-07 13:43:09 +0530

    Update product.router.js

--- router/product.router.js
+++ router/product.router.js
@@ -3,75 +3,83 @@ const proRoute = express.Router();
 
 const Pro = require('../model/product.model');
 
-proRoute.route('/').get(function (req, res) {
-    Pro.find(function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(data);
-        }
+proRoute.route('/').get(function(req, res) {
+  Pro.find(function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+proRoute.route('/').post(function(req, res) {
+  let pro = new Pro(req.body);
+
+  pro
+    .save()
+    .then(obj => {
+      res.status(200).json({ message: 'Product added successfully.' });
+    })
+    .catch(err => {
+      res.status(400).json({ message: 'unable to save the product' });
     });
 });
 
-proRoute.route('/').post(function (req, res) {
-    let pro = new Pro(req.body);
-
-    pro.save().then(obj => {
-        res.status(200).json({ message: 'Product added successfully' });
-    }).catch(err => {
-        res.status(400).json({ message: 'unable to save the product' });
-    });
+proRoute.route('/:id').get(function(req, res) {
+  let id = req.params.id;
+  Pro.findById({ _id: id }, function(err, data) {
+    res.json(data);
+  });
 });
 
-proRoute.route('/:id').get(function (req, res) {
-    let id = req.params.id;
-    Pro.findById({ _id: id }, function (err, data) {
+proRoute.route('/:category').get(function(req, res) {
+  let category = req.params.category;
+  Pro.find({ category:category }, function(err, data) {
+      if(err){
+        throw err;
+      } else {
         res.json(data);
-    });
+      }
+  });
 });
 
-proRoute.route('/:category').get(function (req, res) {
-    let category = req.params.category;
-    Pro.find({ category : category }, function (err, data) {
-        if(err){
-            throw err;
-        } else{
-            res.json(data);
-        }
-    });
+proRoute.route('/:id').patch(function(req, res) {
+  let id = req.params.id;
+  Pro.findById({ _id: id }, function(err, data) {
+    if (!data) {
+      res.status(400).json({ message: 'No data found' });
+    } else {
+      data.title = req.body.title;
+      data.image = req.body.image;
+      data.price = req.body.price;
+      data.category = req.body.category;
+      data.description = req.body.description;
+
+      data
+        .save()
+        .then(obj => {
+          res
+            .status(200)
+            .json({ message: 'Updated product details successfully' });
+        })
+        .catch(err => {
+          res.status(400).json({ message: 'Unable to updated the product' });
+        });
+    }
+  });
 });
 
-proRoute.route('/:id').patch(function (req, res) {
-    let id = req.params.id;
-    Pro.findById({ _id: id }, function (err, data) {
-        if (!data) {
-            res.status(400).json({ message: 'no data found' });
-        } else {
-            data.title = req.body.title;
-            data.image = req.body.image;
-            data.price = req.body.price;
-            data.gst = req.body.gst;
-            data.description = req.body.description;
+proRoute.route('/:id').delete(function(req, res) {
+  let id = req.params.id;
 
-            data.save().then(obj => {
-                res.status(200).json({ message: 'Updated successfully' });
-            }).catch(err => {
-                res.status(400).json({ message: 'Unable to upadate' });
-            });
-        }
-    });
-});
-
-proRoute.route('/:id').delete(function (req, res) {
-    let id = req.params.id;
-
-    Pro.findByIdAndDelete({ _id: id }, function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200).json({ message: 'Successfully Deleted' });
-        }
-    });
+  Pro.findByIdAndDelete({ _id: id }, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).json({ message: 'Successfully Deleted the product' });
+    }
+  });
 });
 
 module.exports = proRoute;
