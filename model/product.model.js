7643d3a096e00403223fb6c6b620be9a1f77de2a
Jovial P Thomas <32453962+jovialp@users.noreply.github.com>
2020-01-07 13:47:56 +0530

    Update product.model.js

--- model/product.model.js
+++ model/product.model.js
@@ -1,21 +1,22 @@
-//db schema
+// db schema
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//declaring collections chema property and names
+// declaring collection schema property and names
 
 let Product = new Schema(
-    {
-        //schema properties
-        title: { type: String },
-        image: { type: String },
-        price: { type: Number },
-        category: { type: String },
-        description: { type: String }
-    },
-    {
-        collection: 'product'
-    }
+  {
+    // schema property: values
+    title: { type: String },
+    image: { type: String },
+    price: { type: Number },
+    category: { type: String },
+    description: { type: String }
+  },
+  {
+    collection: 'product'
+  }
 );
+
 module.exports = mongoose.model('Prod', Product);
